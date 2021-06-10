7a87f253ad68031d610e1a119b1fa42605e8f5f9
gar212 <30423580+gar212@users.noreply.github.com>
2021-06-11 04:43:36 +1200

    text attempt

--- src/components/Background/Background.js
+++ src/components/Background/Background.js
@@ -1,27 +1,31 @@
 import React from 'react';
 import { Canvas, useFrame} from '@react-three/fiber'
 import {OrbitControls, Stars} from "@react-three/drei";
+import Text from '../Text/Text';
 
-function MyAnimatedBox() {
+function RotatingTorus() {
   const myMesh = React.useRef()
+  useFrame(({ clock }) => {
+    myMesh.current.rotation.x = clock.getElapsedTime() /4
+    myMesh.current.rotation.y = clock.getElapsedTime() /3
+  })
   return (
-    <mesh ref={myMesh}>
-      <boxGeometry />
-      <meshBasicMaterial color="royalblue" />
-    </mesh>
+  <mesh ref={myMesh}>
+    <torusGeometry args={[10,3,6,100]} />
+    <meshStandardMaterial />
+  </mesh>
   )
 }
+
 
 export default function Background() {
   return (
     <div id="canvas-container">
       <Canvas>
         <OrbitControls />
-        <mesh>
-          <torusGeometry args={[10,3,6,100]} />
-          <meshStandardMaterial />
-        </mesh>
-        <MyAnimatedBox />
+        <gridHelper />
+        <RotatingTorus />
+        <Text>Garet</Text>
         <Stars count="350" />
         <ambientLight intensity={0.02} color="orange" />
         <directionalLight color="orange" position={[0, 0, 5]} />
