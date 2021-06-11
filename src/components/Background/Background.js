4d25be37e5a0b6e23ea46cf6abe0250f13d6ff98
gar212 <gartoyou@gmail.com>
2021-06-11 17:29:53 +1200

    Create Text Component

--- src/components/Background/Background.js
+++ src/components/Background/Background.js
@@ -1,32 +1,21 @@
 import React from 'react';
-import { Canvas, useFrame} from '@react-three/fiber'
+import { Canvas } from '@react-three/fiber'
 import {OrbitControls, Stars} from "@react-three/drei";
-import Text from '../Text/Text';
-
-function RotatingTorus() {
-  const myMesh = React.useRef()
-  useFrame(({ clock }) => {
-    myMesh.current.rotation.x = clock.getElapsedTime() /4
-    myMesh.current.rotation.y = clock.getElapsedTime() /3
-  })
-  return (
-  <mesh ref={myMesh}>
-    <torusGeometry args={[10,3,6,100]} />
-    <meshStandardMaterial />
-  </mesh>
-  )
-}
-
+import Text from './Text';
 
 export default function Background() {
+
+  function getCameraPosition() {
+    
+  }
+
   return (
     <div id="canvas-container">
-      <Canvas>
+      <Canvas camera={{ position: [0, 0.2, 1] }}>
         <OrbitControls />
         <gridHelper />
-        <RotatingTorus />
-        <Text>Garet</Text>
-        <Stars count="350" />
+        <Text text="Garet Lam" fontSize="2" position={[0,3,-10]} />
+        <Stars count="400" />
         <ambientLight intensity={0.02} color="orange" />
         <directionalLight color="orange" position={[0, 0, 5]} />
       </Canvas>
---
+++ src/components/Background/Text.js
@@ -0,0 +1,33 @@
+import React, { useRef } from 'react'
+import * as THREE from 'three';
+import { useFrame} from '@react-three/fiber'
+import Segoe from '../../styles/font/SegoeUI_Bold.json';
+
+const Text = (props) => {
+  const mesh = useRef(null)
+
+  useFrame(() => {
+    mesh.current.geometry.center()
+  })
+
+  // parse JSON file with Three
+  const font = new THREE.FontLoader().parse(Segoe);
+
+  // configure font geometry
+  const textOptions = {
+    font,
+    size: props.fontSize,
+    height: 0
+  };
+
+
+  return (
+    <mesh position={props.position} ref={mesh}>
+      <textGeometry args={[props.text, textOptions]} />
+      <meshBasicMaterial transparent={true} opacity={0.6} />
+    </mesh>
+  )
+}
+
+
+export default Text
