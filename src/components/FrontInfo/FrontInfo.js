c8d204ab8e08ae7eb53ce23c9ece2bbd845f3eb8
gar212 <gartoyou@gmail.com>
2021-06-27 21:44:50 +1200

    Add basic About Me info

--- src/components/FrontInfo/FrontInfo.js
+++ src/components/FrontInfo/FrontInfo.js
@@ -4,7 +4,11 @@ import './FrontInfo.css';
 const FrontInfo = () => {
     return (
         <div className="front-info-container">
-            <h1>This should be in front</h1>
+            <h1>About Me</h1>
+            <p>25 year old full stack web developer.</p>
+            <p>I have been studying full stack web development through 
+            Mission Ready HQ's tech career accelerator programs, 
+            along with many other full stack orientated online courses.</p>
         </div>
     )
 }
